fafcc341f4d5428d6c5f95a3ecb2cbbb14e2176e
denis909090 <135863448+denis909090@users.noreply.github.com>
2024-01-27 22:52:45 +0200

    start

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// let value = '30'
+// let text = '2021615.023316510 this text can`t be valid'
+
+// console.log(Number(text))
+// console.log(Number(value));
+
+// const num1 = 20;
+// const num2 = 20;
+// console.log(num1 ** num2)
+
+const value = prompt('please, enter number');
+
+console.log(Number(value));
+
+const power = prompt('plese, enter power for number')
+console.log(Number(power));
+
+const result = (value ** power);
+console.log(Number(result));
